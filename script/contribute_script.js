894ee51f3b4078fab81a8d94be10b37e80c524ae
MINJONG JUNG <mjun552@aucklanduni.ac.nz>
2020-10-29 16:05:28 +1300

    Added comments

--- script/contribute_script.js
+++ script/contribute_script.js
@@ -1,14 +1,19 @@
+//This function
 function submit_email(email) {
-	
-  var element = document.getElementById('sort');
+  //When the email is valid
   if (emailIsValid(email) == true){
+	  //Ask user to confirm subscription
 	  var confirm_email = confirm("Would you like to subscribe with \"" + email + "\"?");
+	  //When user confirmed
 	  if (confirm_email != null) alert("Thank you for subscribing")
   }
+  //When user did not enter any input
   else if (email == "") alert("Please enter your email!")
+  //When email is not valid
   else alert("Your email is not valid")
 }
 
+//This function checks validation of email
 function emailIsValid (email) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
