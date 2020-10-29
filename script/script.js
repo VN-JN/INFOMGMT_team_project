95b6b3318e768d3921b67eca133c6c87d7de231e
MINJONG JUNG <mjun552@aucklanduni.ac.nz>
2020-10-29 14:45:40 +1300

    Removed reapeated function

--- script/script.js
+++ script/script.js
@@ -1,116 +1,13 @@
-function closeModal1() {
-    var doc = document.getElementById('product1');
-    doc.style.visibility = 'hidden';
-    doc.style.opacity = 0;
+//This function gets id as parameter to close popup
+function closeModal(ID) {
+  var doc = document.getElementById(ID);
+  doc.style.visibility = 'hidden';
+  doc.style.opacity = 0;
 }
 
-function openModal1() {
-    var doc = document.getElementById('product1');
-    console.log(doc)
-    doc.style.visibility = 'visible'
-    doc.style.opacity = 1
-}
-
-function closeModal2() {
-    var doc = document.getElementById('product2');
-    doc.style.visibility = 'hidden';
-    doc.style.opacity = 0;
-}
-
-function openModal2() {
-    var doc = document.getElementById('product2');
-    console.log(doc)
-    doc.style.visibility = 'visible'
-    doc.style.opacity = 1
-}
-
-function closeModal3() {
-    var doc = document.getElementById('product3');
-    doc.style.visibility = 'hidden';
-    doc.style.opacity = 0;
-}
-
-function openModal3() {
-    var doc = document.getElementById('product3');
-    console.log(doc)
-    doc.style.visibility = 'visible'
-    doc.style.opacity = 1
-}
-
-function closeModal4() {
-    var doc = document.getElementById('product4');
-    doc.style.visibility = 'hidden';
-    doc.style.opacity = 0;
-}
-
-function openModal4() {
-    var doc = document.getElementById('product4');
-    console.log(doc)
-    doc.style.visibility = 'visible'
-    doc.style.opacity = 1
-}
-
-function closeModal5() {
-    var doc = document.getElementById('product5');
-    doc.style.visibility = 'hidden';
-    doc.style.opacity = 0;
-}
-
-function openModal5() {
-    var doc = document.getElementById('product5');
-    console.log(doc)
-    doc.style.visibility = 'visible'
-    doc.style.opacity = 1
-}
-
-function closeModal6() {
-    var doc = document.getElementById('product6');
-    doc.style.visibility = 'hidden';
-    doc.style.opacity = 0;
-}
-
-function openModal6() {
-    var doc = document.getElementById('product6');
-    console.log(doc)
-    doc.style.visibility = 'visible'
-    doc.style.opacity = 1
-}
-
-function closeModal7() {
-    var doc = document.getElementById('product7');
-    doc.style.visibility = 'hidden';
-    doc.style.opacity = 0;
-}
-
-function openModal7() {
-    var doc = document.getElementById('product7');
-    console.log(doc)
-    doc.style.visibility = 'visible'
-    doc.style.opacity = 1
-}
-
-function closeModal8() {
-    var doc = document.getElementById('product8');
-    doc.style.visibility = 'hidden';
-    doc.style.opacity = 0;
-}
-
-function openModal8() {
-    var doc = document.getElementById('product8');
-    console.log(doc)
-    doc.style.visibility = 'visible'
-    doc.style.opacity = 1
-}
-
-function closeModal9() {
-    var doc = document.getElementById('product9');
-    doc.style.visibility = 'hidden';
-    doc.style.opacity = 0;
-}
-
-function openModal9() {
-    var doc = document.getElementById('product9');
-    console.log(doc)
-    doc.style.visibility = 'visible'
-    doc.style.opacity = 1
+//This function gets id as parameter to open popup
+function openModal(ID) {
+  var doc = document.getElementById(ID);
+  doc.style.visibility = 'visible';
+  doc.style.opacity = 1;
 }
